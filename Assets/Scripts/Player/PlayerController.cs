5a09e4678fbc7598c413a30fe9407947fc5045cb
Daria-Romanko <Diagic1000@gmail.com>
2023-10-16 16:38:41 +0300

    Fix psb file bug

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,18 +8,18 @@ public class PlayerController : MonoBehaviour
 
     private float moveInput;
 
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D playerRigidbody;
     private bool fasingRight;
 
     private void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
         moveInput = Input.GetAxis("Horizontal");
-        rigidbody.velocity = new Vector2(moveInput * speed, rigidbody.velocity.y);
+        playerRigidbody.velocity = new Vector2(moveInput * speed, playerRigidbody.velocity.y);
         if (fasingRight == false && moveInput > 0)
         {
             Flip();
