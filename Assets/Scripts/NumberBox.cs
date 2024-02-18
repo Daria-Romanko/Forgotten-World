681b9e20575ceab98ac6084262ee859d6cd51d83
Daria-Romanko <115109486+Daria-Romanko@users.noreply.github.com>
2024-02-18 17:28:00 +0300

    Add ClickToSwap for puzzle15

--- Assets/Scripts/NumberBox.cs
+++ Assets/Scripts/NumberBox.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,34 @@ public class NumberBox : MonoBehaviour
     public int index;
     int x = 0, y = 0;
 
+    private Action<int, int> swapFunc = null;
 
-    public void Init(int i,int j,int index,Sprite sprite)
+    public void Init(int i, int j, int index, Sprite sprite, Action<int,int> swapFunc)
     {
-        this.index= index;
-        this.GetComponent<SpriteRenderer>().sprite= sprite;
+        this.index = index;
+        this.GetComponent<SpriteRenderer>().sprite = sprite;
         UpdatePos(i, j);
+        this.swapFunc = swapFunc;
     }
-    public void UpdatePos(int i,int j)
+    public void UpdatePos(int i, int j)
     {
         x = i;
         y = j;
+        this.gameObject.transform.SetParent(transform.parent);
         this.gameObject.transform.localPosition = new Vector2(i, j);
     }
+
+    public bool IsEmpty()
+    {
+        return index == 16;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0) && swapFunc != null)
+        {
+            swapFunc(x, y);
+        }
+    }
+
 }
--- Assets/Scripts/puzzle.cs
+++ Assets/Scripts/puzzle.cs
@@ -20,10 +20,55 @@ public class puzzle : MonoBehaviour
         for(int y=3;y>=0;y--)
             for(int x=0;x<4;x++)
             {
-                NumberBox box = Instantiate(boxPrefab, new Vector2(x, y), Quaternion.identity);
-                box.Init(x, y, n + 1, sprites[n]);
+                NumberBox box = Instantiate(boxPrefab, transform.position, Quaternion.identity, gameObject.transform);
+                box.Init(x, y, n + 1, sprites[n], ClickToSwap);
                 n++;
                 boxes[x, y] = box;
             }
+    }
+
+    void ClickToSwap(int x, int y)
+    {
+        int dx = getDx(x, y);
+        int dy = getDy(x, y);
+
+        var from = boxes[x, y];
+        var target = boxes[x+dx, y+dy];
+
+        boxes[x, y] = target;
+        boxes[x + dx, y + dy] = from;
+
+        from.UpdatePos(x + dx, y + dy);
+        target.UpdatePos(x, y);
+    }
+
+    int getDx(int x, int y)
+    {
+        if(x < 3 && boxes[x + 1, y].IsEmpty())
+        {
+            return 1;
+        }
+
+        if (x > 0 && boxes[x - 1, y].IsEmpty())
+        {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    int getDy(int x, int y)
+    {
+        if (y < 3 && boxes[x, y + 1].IsEmpty())
+        {
+            return 1;
+        }
+
+        if (y > 0 && boxes[x, y - 1].IsEmpty())
+        {
+            return -1;
+        }
+
+        return 0;
     }
 }
